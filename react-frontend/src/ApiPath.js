4aa816a912af60cd9b7413b33105551da901957b
Ali Madad <ali@scty.org>
2023-12-26 21:28:43 -0500

    cleanup

--- react-frontend/src/ApiPath.js
+++
@@ -1,3 +0,0 @@
-const ApiPath = import.meta.env.VITE_API_PATH
-
-export default ApiPath
--- react-frontend/src/stores/index.js
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand'
-import { chatStore } from './ChatStore'
-
-export const useBoundStore = create((...a) => ({
-  ...chatStore(...a),
-}))
